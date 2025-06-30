de0c952ce7efff0576321182d9d080cfb88f4238
NiranjanLearning1 <niranjanreddyanugu.learning@gmail.com>
2025-06-30 17:26:53 +0530

    Very Basic Node.js API

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) =>{
+res.send("Hello! This is Niranjan Reddy Anugu.")
+});
+
+app.get("/greet", (req, res) =>{
+res.json({"message": "Welcome to the Node.js API!"});
+});
+
+app.listen(PORT, ()=>{
+console.info(`Server is Running on PORT: ${PORT}`);
+});
